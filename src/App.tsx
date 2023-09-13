9621f303ac304647fd00b1189ea71d0983ecd114
GioZhorzholiani3 <giorgi.zhorzholiani.1@btu.edu.ge>
2023-09-13 16:42:14 +0400

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+
 function App() {
-  return <div>app</div>;
+  const [selectedPage, setSelectedPage] = useState<string>("Home");
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
